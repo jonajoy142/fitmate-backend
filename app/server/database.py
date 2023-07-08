5020ef8fada045747873ab341ed066b66bf17836
Deepak M S <deepakcoder80@gmail.com>
2023-07-08 10:30:06 +0530

    database connectivity file

---
+++ app/server/database.py
@@ -0,0 +1,32 @@
+"""
+Database connectivity file
+"""
+from dotenv import load_dotenv, find_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+import os
+
+# get enviornment variables
+load_dotenv(find_dotenv())
+
+host = os.getenv("DATABASE_HOST")
+username = os.getenv('DATABASE_USER_NAME')
+password = os.getenv('DATABASE_PASSWORD')
+database = os.getenv('DATABASE')
+
+# database url
+DATABASE_URL = f"postgresql://{username}:{password}@{host}/{database}"
+
+# get database connect via engine
+engine = create_engine(DATABASE_URL)
+
+# local session creator for database
+local_session = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+)
+
+# base to create models to database
+base = declarative_base()
