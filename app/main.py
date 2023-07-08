691215abd1d29dd45bbfcb1db047fd6405f76625
Deepak M S <deepakcoder80@gmail.com>
2023-07-08 10:22:07 +0530

    basic setup

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+# main router
+app = FastAPI(title="FITMATE API", version=0.1,)
+
+# root 
+@app.get(path="/",tags=["Root Route"])
+def read_root():
+    """
+    read the root of the api
+    :return : status of the api
+    """
+    return {"status":"OK","message":"Available to integrate"}
