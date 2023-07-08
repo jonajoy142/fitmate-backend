7ebf8a1b69051a90a8276b8126cd5014979a3ad0
Deepak M S <deepakcoder80@gmail.com>
2023-07-08 11:06:14 +0530

    tables created

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+import sys
+sys.path.append("../")
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from app.server.database import engine
+import app.server.models as models
 
 # main router
 app = FastAPI(title="FITMATE API", version=0.1)
+
+#create all tables in the database
+models.base.metadata.create_all(bind=engine)
 
 # root 
 @app.get(path="/",tags=["Root Route"])
---
+++ app/server/models.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append("../../")
+
+from app.server.database import base
+from sqlalchemy import Column,String,Integer,Float,DateTime,ForeignKey
+from datetime import datetime
+
+class User(base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True,autoincrement=True,index=True)
+    name = Column(String, index=True, nullable=False)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow,
+                        onupdate=datetime.utcnow)
+
+class BioMetrix(base):
+    __tablename__ = "user_biomatrix"
+    id = Column(Integer,primary_key=True,autoincrement=True,index=True)
+    user_id = Column(Integer,ForeignKey("users.id"), nullable=False)
+    age = Column(Integer)
+    height = Column(Float)
+    weight = Column(Float)
+
+class FoodItems(base):
+    __tablename__ = "food_items"
+    id = Column(Integer,primary_key=True,autoincrement=True,index=True)
+    user_id = Column(Integer,ForeignKey("users.id"), nullable=False)
+    food_name = Column(String)
+    food_type = Column(String)
+    quantity = Column(Float)
+    calories = Column(Float)
+    food_time = Column(DateTime)
+    
